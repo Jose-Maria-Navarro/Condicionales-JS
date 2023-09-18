4521add970e6ba2b125f3f7cdeed549554c71e2c
Jose-Maria-Navarro <144720622+Jose-Maria-Navarro@users.noreply.github.com>
2023-09-18 12:08:41 -0600

    Detectar numero menor

--- Ejercicio 5.js
+++ Ejercicio 5.js
@@ -1,1 +1,19 @@
-Prueba
+
+let numero1
+let numero2
+let respuesta
+
+numero1 = prompt("Intruduce el primer numero")
+numero2 = prompt("Intruduce el segundo numero")
+
+if(numero1 < numero2){
+  respuesta = "El numero "+numero1+" es menor que el numero "+numero2
+
+}else if(numero1 > numero2){
+  respuesta = "El numero "+numero2+" es menor que el numero "+numero1
+
+
+}
+
+
+console.log(respuesta)
