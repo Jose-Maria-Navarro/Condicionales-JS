6157b2ba95198659b96855de45fc9b40882f36ab
Jose-Maria-Navarro <144720622+Jose-Maria-Navarro@users.noreply.github.com>
2023-09-18 21:09:56 -0600

    Dias de la semana

---
+++ Ejercicio 7.js
@@ -0,0 +1,37 @@
+//Requerir al usuario que ingrese un día de la semana e imprimir un mensaje si es lunes, otro mensaje diferente si es viernes, otro mensaje diferente si es sábado o domingo. Si el día ingresado no es ninguno de esos, imprimir otro mensaje.
+
+let dia
+
+
+dia = prompt("Escribe un dia de la semana")
+
+if(dia == "lunes"){
+console.log("Este es el primer dia de la semana")
+
+}else if(dia == "martes"){
+console.log("Este es el segundo dia de la semana")
+
+}else if(dia == "martes"){
+console.log("Este es el tercer dia de la semana")
+
+}else if(dia == "martes"){
+console.log("Este es el cuarto dia de la semana")
+
+}else if(dia == "martes"){
+console.log("Este es el quinto dia de la semana")
+
+}else if(dia == "martes"){
+console.log("Este es el sexto dia de la semana")
+
+}else if(dia == "martes"){
+console.log("Este es el septimo dia de la semana")
+
+}else{
+console.log("No escribiste un dia valido")
+
+}
+
+
+
+
+
